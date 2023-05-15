59fc584e347fc914bc1d8c8bc4acfe7286100204
Pavel Mudrov <pavel.s.mudrov@gmail.com>
2023-05-15 15:34:34 +0200

    Qp

--- codewars_2.js
+++ codewars_2.js
@@ -527,3 +527,47 @@ function getMiddle(s)
     }
 }
 console.log(getMiddle('testing'));
+
+//hz
+function countPositivesSumNegatives(input) {
+    let result = [0,0];
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] > 0) {
+        result[0] += 1;
+      } else {
+        result[1] += input[i];
+      }
+    }
+    return result;
+  }
+  console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+
+  //hz
+  function greet(language) {
+    const lng = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+
+  if(lng[language] != undefined) {
+        return lng[language];
+    } else {
+        return lng.english;
+    }
+}
+console.log(greet('dutchdsdsd'));
